40c8f1469eef9511d962f2f6df12fefa48626402
tyler-gotz <tyler.gotz@teschglobal.com>
2023-03-13 11:25:28 -0500

    about and experience pages done

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
+import { Suspense, useEffect, useState } from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from '../Loader'
 
-const Computers = () => {
+const Computers = ({ isMobile }) => {
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15} groundColor='black' />
+      <pointLight intensity={1} />
+      <spotLight 
+        position={[-20, 50, 10]} 
+        angle={0.12} 
+        penumbra={1}
+        intensity={1}
+        castShadow
+        shadow-mapSize={1024}
+      />
+      <primitive
+        object={computer.scene}
+        scale={isMobile ? .7 : .75}
+        position={isMobile ? [0, -3, -2.2] : [0, -3.75, -1.25]}
+        rotation={[-0.01, -0.2, -0.1]}
+      />
+    </mesh>
   )
 }
 
-export default Computers
+const ComputerCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false)
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)')
+
+    setIsMobile(mediaQuery.matches)
+
+    const handleQueryChange = (event) => {
+      setIsMobile(event.matches)
+    }
+
+    mediaQuery.addEventListener('change', handleQueryChange)
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleQueryChange)
+    }
+  }, [])
+
+  return (
+    <Canvas
+      frameloop='demand'
+      shadows
+      camera={{ position: [20, 3, 5], fov: 25 }}
+      gl={{ preserveDrawingBuffer: true }}
+    >
+      <Suspense fallback={<CanvasLoader />}>
+        <OrbitControls 
+          enableZoom={false} 
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+        />
+        <Computers isMobile={isMobile} />
+      </Suspense>
+      <Preload all />
+    </Canvas>
+  )
+}
+
+export default ComputerCanvas
