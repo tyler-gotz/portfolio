a66e452ed7d3e883edc0617178d45a32132027ee
Tyler Gotz <tyler.gotz@teschglobal.com>
2026-01-18 19:51:51 -0600

    responsive fixes

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -4,7 +4,7 @@ import React, { useRef, useState } from "react";
 
 const Projects: React.FC = () => {
   const ref = useRef(null);
-  const isInView = useInView(ref, { once: true, amount: 0.2 });
+  const isInView = useInView(ref, { once: true, amount: 0.1 });
   const [hoveredIndex, setHoveredIndex] = useState<number | null>(null);
 
   const projects = [
